0cd964b75c58152fe626d1153041745d03b8e599
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2024-07-06 19:06:50 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# AWS Lambda in Rust
+
+This project demonstrates how to build and deploy an AWS Lambda function using Rust. 
+The Lambda function generates a UUID, stores it in an S3 bucket and a DynamoDB table, and returns the UUID in a JSON response.
+
+## Prerequisites
+
+- Install [Rust](https://www.rust-lang.org/) and [Cargo](https://doc.rust-lang.org/cargo/).
+- Install [AWS CLI](https://aws.amazon.com/cli/).
+
+## Steps
+
+### set up IAM role & permissions
+
+install musl
+
+### 1. install Rust & Cargo
+    ```bash
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    ```
+
+### 2. install `cargo-lambda`, a tool that simplifies building and deploying AWS Lambda functions written in Rust
+
+    ```bash
+    cargo install cargo-lambda
+    ```
+
+### 3. the cargo.toml file: 
+this lists the dependencies for the project. Its very similar to the pyproject.toml you may use if you choose Poetry as 
+your python dependency manager. It probably feels a little different if you're used to using pip or conda
+
+### 4. the main.rs file: this is our lambda function, it performs the following functions
+Your AWS Lambda function written in Rust performs the following tasks:
+    - **Generate a UUID**: It creates a unique identifier (UUID) which will be used as a unique identifier for the objects being stored.
+    - **Store Data in S3**: The UUID is converted to bytes and stored in an S3 bucket as a file with the UUID as the filename.
+    - **Store Data in DynamoDB**: The UUID is also stored in a DynamoDB table as an item with the key "id" set to the UUID value.
+    - **Return a JSON Response**: Finally, it returns a JSON response containing the UUID.
+serves as an example of integrating AWS services (S3 and DynamoDB) using Rust, demonstrating basic operations like 
+storing and retrieving data. Its not exciting, but serves as a basis for comparison
+
+### 5. compile the lambda function
+make sure you specify --release to reduce the size of the binary
+Don't use --debug, it will make the binary too large
+```bash
+cargo clean
+```
+
+```bash
+cargo lambda build --release --target x86_64-unknown-linux-musl
+```
+
+we can further reduce the size of the binary by stripping it
+```bash
+strip target/lambda/aws_rust/bootstrap
+```
+
+### 6. package the built lambda function into a zip file
+
+```bash
+cd target/lambda/aws_rust
+zip lambda_function.zip bootstrap
+```
+
+### 7. deploy the lambda function using the AWS CLI
+new
+```bash
+cd target/lambda/aws_rust
+aws lambda create-function --function-name rust-lambda \
+    --runtime provided.al2 --role arn:aws:iam::001499655372:role/Rust-vs-Python-project \
+    --handler bootstrap --zip-file fileb://lambda_function.zip
+```
+
+update
+```bash
+cd target/lambda/aws_rust
+aws lambda update-function-code --function-name rust-lambda --zip-file fileb://lambda_function.zip
+```
+
+### 9. invoke the lambda function
+```bash
+aws lambda invoke --function-name rust-lambda output.json
+```
+
+check the response
+```bash
+cat output.json
+```
--- src/rust_lambda.rs
+++ src/rust_lambda.rs
@@ -6,10 +6,14 @@ use aws_sdk_dynamodb::Client as DynamoDbClient;  // AWS SDK client for DynamoDB
 use aws_sdk_dynamodb::types::AttributeValue; // AWS SDK types for DynamoDB attribute values
 use std::env;  // for accessing environment variables
 use uuid::Uuid;  // to generate UUIDs
-
+use log::{info, error};  // For logging
+use simple_logger::SimpleLogger;  // For simple logging setup
 
 #[tokio::main]  // the main function is the entry point of the Lambda function
 async fn main() -> Result<(), Error> {  // main function returns a Result or an Error
+    // Initialize the logger
+    SimpleLogger::new().init().unwrap();
+
     let func = handler_fn(my_handler);  // create handler function
     lambda_runtime::run(func).await?;  // run Lambda runtime with handler function
     Ok(())  // return Ok if successful, else return Error
@@ -18,24 +22,52 @@ async fn main() -> Result<(), Error> {  // main function returns a Result or an 
 // the handler function that is executed when the Lambda function is invoked
 async fn my_handler(_: serde_json::Value, _: Context) -> Result<serde_json::Value, Error> {
     // read env vars for S3 bucket name and DynamoDB table name
-    let s3_bucket_name = env::var("S3_BUCKET_NAME").expect("S3_BUCKET_NAME not set");  // expect() panics if the variable is not set
-    let dynamo_table_name = env::var("DYNAMODB_TABLE").expect("DYNAMODB_TABLE not set");  // expect() panics if the variable is not set
-    let config = aws_config::from_env().load().await;  // load AWS config from env
+    let s3_bucket_name = match env::var("S3_BUCKET_NAME") {
+        Ok(val) => val,
+        Err(e) => {
+            error!("S3_BUCKET_NAME not set: {}", e);
+            return Err(e.into());
+        }
+    };
+
+    let dynamo_table_name = match env::var("DYNAMODB_TABLE") {
+        Ok(val) => val,
+        Err(e) => {
+            error!("DYNAMODB_TABLE not set: {}", e);
+            return Err(e.into());
+        }
+    };
+
+    let config = aws_config::load_from_env().await;  // load AWS config from env
     let s3_client = S3Client::new(&config);  // create S3 and DynamoDB clients using the loaded config
     let dynamo_client = DynamoDbClient::new(&config);
+
     let guid = Uuid::new_v4().to_string();  // generate new UUID
     let file_name = format!("{}.txt", guid); // create file name using UUID
     let encoded_string = guid.clone().into_bytes();  // encode UUID as bytes
 
     // put the encoded UUID into the S3 bucket
-    s3_client.put_object().bucket(&s3_bucket_name).key(&file_name).body(encoded_string.into()).send().await?;  // send the request to S3 and wait for the response
+    match s3_client.put_object().bucket(&s3_bucket_name).key(&file_name).body(encoded_string.into()).send().await {
+        Ok(_) => info!("Successfully put object in S3"),
+        Err(e) => {
+            error!("Failed to put object in S3: {}", e);
+            return Err(e.into());
+        }
+    };
 
     // put the UUID into the DynamoDB table
-    dynamo_client.put_item()
+    match dynamo_client.put_item()
         .table_name(&dynamo_table_name)  // specify the table name
         .item("id", AttributeValue::S(guid.clone()))  // specify the item to be inserted
         .send()  // send the request
-        .await?;  // wait for the response
+        .await {
+        Ok(_) => info!("Successfully put item in DynamoDB"),
+        Err(e) => {
+            error!("Failed to put item in DynamoDB: {}", e);
+            error!("DynamoDB Error: {:?}", e);
+            return Err(e.into());
+        }
+    };
 
     Ok(json!({"id": guid}))  // return a JSON response with the UUID
 }
